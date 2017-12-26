c408555bc61c65e6853151d93e597a886a41afe1
Dariusz Rosiek <daro.krk@interia.pl>
2017-12-26 16:15:59 +0100

    Change content in js/script.js, triangle area calculations function

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// traingle field
+
+/*
+var a = 20,
+	h = 10,
+	triangleArea;
+
+triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+*/
+
+var a = prompt('Enter a value of triangle:'),
+	h = prompt('Enter height value of triangle:'),
+	triangleArea;
+
+triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to : ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to : ' + triangleArea);
